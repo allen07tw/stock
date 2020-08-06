e74d845467080cf83ddb5e9cdb5de833f42e8787
allen <allen07tw@gmail.com>
2020-08-06 11:50:13 +0800

    file search
    funtion
    this fuction is used to search data of someday
    and you can choose what stock number and feature you want to watch
    furthermore ,you can also see the 14 ago  data including the day you choose
    
    technique
    in this code we use 'sorted' to sort the data for dictionary 'index'
    function, and we also use 'with open','appends'(this tech we have to
    create output = [] to add) ,'listdir'(to catch data from some dir but you
    have to import the os library at first)
    in addtion,we also can use'glob variable' if the name is too long or use
    it for too many time
    'if'and 'in' is a  good way to choose whether the funcrion active or not
    how to take a data from a dictionary is also a important thing in this
    program we take
    		want_file = json.loads(f.read())
    		output.append(want_file[stock_id][feature])
    for example
    
    if we want to use json.'load'function we must import json library
    
    reference
    https://www.runoob.com/python/os-listdir.html
    https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory
    https://stackoverflow.com/questions/22412509/getting-a-sublist-of-a-python-list-with-the-given-indices
    https://book.pythontips.com/en/latest/context_managers.html

--- stock.py
+++ stock.py
@@ -1,57 +1,30 @@
 #! /usr/bin/env python3
-# source dir: /home/db/stock_resource_center/resource/twse/json
-
-# get_sample("0050", "2020-02-01", "close")
-# 1. list all files in directory
-# 2. load json file
-# 3. sorting
-# 4. list subindex
 import json
 import os
-
-'''
-#print(type(sdata))
-# data = json.loads(sdata[])
-#
-#print(sdata)
-
-#jf = json.load(open(path+"2004-04-23.json"))
-
-#print(jf['0050'])
-
-for f in sdata:
-    with open (path+f,'r') as reader:
-        jf = json.loads(reader.read())
-        print(jf['0050']['close'])
-'''
-#print(jf['stock_id']['data']['feature'])
 
 PARENT_DIR = "/home/db/stock_resource_center/resource/twse/json"
 
 def format_date(date):
-    return PARENT_DIR + date + ".json"
+    return PARENT_DIR + "/" + date + ".json"
 
-def get_14day_sample(date, stick_id, feature):
-    if format_date(date) in sdata:
-        for f in sdata:
-             with open (path+f,'r') as reader:
-                 jf = json.loads(reader.read())
-        ed = sdata.index("/home/db/stock_resource_center/resource/twse/json" + date + ".json")
-        sd = ed-14
-        jf=sorted(jf)
-        return print(jf[sd:ed+1]['stock_id']['feature'])
+def get_14day_sample(file_data, date, stock_id, feature):
+    if date+".json" in file_data:
+        end = file_data.index(date + ".json")
+        start = end-14
+        print(end,start)
+        print(file_data[start:end+1])
+        output = []
+        for stock_data in file_data[start:end+1]:
+            with open ( PARENT_DIR +"/"+ stock_data,'r') as f:
+                want_file = json.loads(f.read())
+                output.append(want_file[stock_id][feature])
+        return output
 
 def main():
-    sdata = os.listdir(PARENT_DIR)
-    get_14day_sample("2020-08-03", "5050", "close")
+    file_data = os.listdir(PARENT_DIR)
+    file_data = sorted(file_data)
+    get_14day_sample(file_data,"2020-08-03", "0050", "close")
+    print(get_14day_sample(file_data,"2020-08-03", "0050", "close"))
 
 if __name__ == "__main__":
     main()
-
-
-#def get_sample(stock_id, date, feature):
- #   data = listdir(/home/db/stock_resource_center/resource/twse/json)
-
-
-  #  return [10.5, 12.5, 13.4 ...] # 14 value
-
